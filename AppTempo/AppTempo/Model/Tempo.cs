14e555d4b7865301e7252cdca4841e82e960ed6f
Usuario <Usuario@CPD03-M05>
2022-09-27 11:59:35 -0300

    .

--- AppTempo/AppTempo/Model/Tempo.cs
+++ AppTempo/AppTempo/Model/Tempo.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace AppTempo.Model
 {
-    internal class Tempo
+     internal class Tempo
     {
+        public string Title { get; set; }
+        public string Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Humidity { get; set; }
+        public string Visibility { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunsut { get; set; }
     }
+
+       public Tempo()
+    {
+        this.Title = " ";
+        this.Temperature = " ";
+        this.Wind = " ";
+        this.Humidity = " ";
+        this.Visibility = " ";
+        this.Sunrise = " ";
+        this.Sunset = " ";
+    }
+
 }
