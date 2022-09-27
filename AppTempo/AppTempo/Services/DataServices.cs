78b436f3cc1865dfe974b85c1526c1b627f768dc
Usuario <Usuario@CPD03-M05>
2022-09-27 12:14:06 -0300

    .

--- AppTempo/AppTempo/Services/DataServices.cs
+++ AppTempo/AppTempo/Services/DataServices.cs
@@ -2,9 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppTempo.Model;
+
 namespace AppTempo.Services
 {
     internal class DataServices
     {
+      public static async Task<Tempo> GetPrevisaoDoTempo (string cidade)
+        {
+            string appId = "";
+
+            string queryString = "" + cidade + "&units=metric" + "&appid=" + appId;
+
+            dynamic resultado = await getDataFromServices(queryString).ConfigureAwait(false);
+
+            if (resultado["weather"] != null)
+            {
+
+            }
+
+        }
+
+        
+
     }
 }
