bad215267d20ae8eccb4d39f8fea8ec697d48cf0
Usuario <Usuario@CPD03-M05>
2022-09-27 12:27:48 -0300

    .

--- AppTempo/AppTempo/Services/DataServices.cs
+++ AppTempo/AppTempo/Services/DataServices.cs
@@ -13,7 +13,7 @@ namespace AppTempo.Services
     internal class DataServices
     {
       public static async Task<Tempo> GetPrevisaoDoTempo (string cidade)
-        {
+      {
             string appId = "";
 
             string queryString = "" + cidade + "&units=metric" + "&appid=" + appId;
@@ -22,10 +22,25 @@ namespace AppTempo.Services
 
             if (resultado["weather"] != null)
             {
-
+                Tempo previsao = new Tempo();
+                previsao.Title = (string)resultado["name"];
+                previsao.Temperature = (string)resultado["main"]["temp"] + "C";
+                previsao.Wind = (string)resultado["wind"]["speed"] + "mph";
+                previsao.Humidity = (string)resultado["main"]["humidity"] + "%";
+                previsao.Visibility = (string)resultado["weather"][0]["main"];
+                DateTime time = new DateTime (1970,1, 1,0,0,0,0);
+                DateTime sunrise = time.AddSeconds((double)resultado["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)resultado["sys"]["sunset"]);
+                previsao.Sunrise = String.Format("{0: d/MM/yyyy HH:mm:ss}", sunrise);
+                previsao.Sunset = String.Format("{0: d/MM/yyyy HH:mm:ss}", sunset);
+                return previsao;
+            }
+            else
+            {
+                return null;
             }
 
-        }
+      }
 
         
 
